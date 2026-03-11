f2ebed0ca52a7d54e4159c544efcdbc3100163c2
Sergio Robles <srobles-i001r@kinal.edu.gt>
2026-03-10 21:02:28 -0600

    Agregando google client

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@
  */
 const CONFIG = {
   API_BASE_URL: 'https://script.google.com/macros/s/AKfycbzlYnlq-VHvalnY7YyijTaJwkZdINhP8u_hKpxEzz07icNVtPlMHbQnbgKa8f_E6YfB/exec',
-  GOOGLE_CLIENT_ID: 'TU_GOOGLE_CLIENT_ID.apps.googleusercontent.com',
+  GOOGLE_CLIENT_ID: '102964991939-8kidn5pvhaumcrhrrnnnadctuuhag4ip.apps.googleusercontent.com',
   TAX_RATE: 0.12,
   TAX_NAME: 'IVA 12%',
   CURRENCY: 'Q',
